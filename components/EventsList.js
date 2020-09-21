f7ae8ba4a4e5f8f94bb0447b8c587f5f69117deb
Victor Kariuki <karizvic@gmail.com>
2020-09-21 13:27:22 +0300

    use simple grid for responsive design

--- components/EventsList.js
+++ components/EventsList.js
@@ -1,14 +1,14 @@
-import { Grid } from '@chakra-ui/core'
+import { SimpleGrid } from '@chakra-ui/core'
 
 import Event from './Event'
 
 const EventsList = ({ events }) => {
   return (
-    <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={10}>
+    <SimpleGrid columns={{sm: 2, md: 3}} spacing={10}>
       {events.map(event => (
         <Event key={event.id} {...event}/>
       ))}
-    </Grid>
+    </SimpleGrid>
   )
 }
 
